2e85dad773a275f791e2b73fafdb23f392d00e70
Jeremy Bennett <jeremylukebennett@comcast.net>
2018-10-29 17:33:15 -0500

    solved

--- main.js
+++ main.js
@@ -1,39 +1,46 @@
 
 
+// This function takes a string with a single number in each word (as strings) and returns the string with the words rearranged in the order of the numbers embedded into the words. ie: "is2 Thi1s T4est 3a" becomes -->  "Thi1s is2 3a T4est".
+
 function order(words){
 
-// Divides the words in the string into array elements
-words = words.split(" ");
-// console.log(words);
+    
+    let newWordsArray = [];
+    console.log(words);
+    
+    if(words != "") {
+        
+        // Divides the words in the string into array elements
+        words = words.split(" ");
 
-let newWordsArray = [];
+        for (let i = 0; i < words.length; i++) {
+            
 
+            // This regular expression extracts the digits in each string, targeting them with the [0] index number (The code below returns an object, and the first item is the digit).
+            let digitFinder = /\d+/;
+            // console.log("Extracted number as string: ", words[i].match(digitFinder)[0]);
 
-for (let i = 0; i < words.length; i++) {
+            // This finds the number in each string in the array and converts it to an actual number (instead of a string)
+            let currentRank = parseInt(words[i].match(digitFinder)[0]);
 
-    let digitFinder = /\d+/;
+            // console.log('currentRank',currentRank);
+            // console.log('words[i]',words[i]);
 
-    console.log(words[i].match(digitFinder));
+            newWordsArray[currentRank - 1] = words[i];
+        }
 
-    // This finds the number in each string in the array and converts it to an actual number (instead of a string)
-    let currentRank = parseInt(words[i].match(digitFinder)[0]);
+        newWordsArray = newWordsArray.join(" ");
+        // console.log('newWordsArray',newWordsArray);
 
-    console.log('currentRank',currentRank);
-    console.log('words[currentRank - 1]',words[currentRank - 1]);
+        // console.log('newWordsArray',newWordsArray);
+        return newWordsArray;
+    }
+    else {
+        return "";
+    }
 
-    newWordsArray[i] = words[currentRank - 1];
 }
 
-newWordsArray = newWordsArray.join(" ");
-console.log('newWordsArray',newWordsArray);
 
-
-return newWordsArray;
-
-
-
-}
-
-
-//   order("is2 Thi1s T4est 3a");
-order("4of Fo1r pe6ople g3ood th5e the2");
+  order("is2 Thi1s T4est 3a");
+// order(" ");
